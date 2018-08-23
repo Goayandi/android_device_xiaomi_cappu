3cf7d9f75f63038d090e9397d9dcaee55268f5b6
goayandi <r.borutha@gmail.com>
2018-08-23 19:45:33 +0200

    Some changes

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo lineage_xds-user
-add_lunch_combo lineage_xds-userdebug
-add_lunch_combo lineage_xds-eng
+add_lunch_combo lineage_cappu-user
+add_lunch_combo lineage_cappu-userdebug
+add_lunch_combo lineage_cappu-eng
