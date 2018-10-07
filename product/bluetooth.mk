8e4ed831ad149b173e41398d06acc5e022e86fd7
goayandi <r.borutha@gmail.com>
2018-10-07 23:21:58 +0200

    bluetooth: additional packages and configs

--- product/bluetooth.mk
+++ product/bluetooth.mk
@@ -3,3 +3,13 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-service \
 	libbluetooth_mtk \
     libbt-vendor
+
+PRODUCT_COPY_FILES += \
+	$(DEVICE_PATH)/configs/bluetooth/auto_pair_blacklist.conf:system/vendor/etc/bluetooth/auto_pair_blacklist.conf \
+	$(DEVICE_PATH)/configs/bluetooth/auto_pair_devlist.conf:system/vendor/etc/bluetooth/auto_pair_devlist.conf \
+	$(DEVICE_PATH)/configs/bluetooth/btconfig.xml:system/vendor/etc/bluetooth/btconfig.xml \
+	$(DEVICE_PATH)/configs/bluetooth/bt_did.conf:system/vendor/etc/bluetooth/bt_did.conf \
+	$(DEVICE_PATH)/configs/bluetooth/bt_stack.conf:system/vendor/etc/bluetooth/bt_stack.conf \
+	$(DEVICE_PATH)/configs/bluetooth/bt_stack.conf.debug:system/vendor/etc/bluetooth/bt_stack.conf.debug \
+	$(DEVICE_PATH)/configs/bluetooth/bt_stack.conf.sqc:system/vendor/etc/bluetooth/bt_stack.conf.sqc
+
