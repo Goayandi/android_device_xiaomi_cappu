433bfebd59e7804081ae346128b8795e57be173e
goayandi <r.borutha@gmail.com>
2018-10-07 22:53:30 +0200

    ramdisk.mk: clean up

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -9,9 +9,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.mt8173.rc:root/init.mt8173.rc \
     $(LOCAL_PATH)/rootdir/init.mt8173.usb.rc:root/init.mt8173.usb.rc \
     $(LOCAL_PATH)/rootdir/init.project.rc:root/init.project.rc \
-    $(LOCAL_PATH)/rootdir/meta_init.connectivity.rc:root/meta_init.connectivity.rc \
-    $(LOCAL_PATH)/rootdir/meta_init.modem.rc:root/meta_init.modem.rc \
-    $(LOCAL_PATH)/rootdir/meta_init.project.rc:root/meta_init.project.rc \
-    $(LOCAL_PATH)/rootdir/meta_init.rc:root/meta_init.rc \
     $(LOCAL_PATH)/rootdir/ueventd.mt8173.rc:root/ueventd.mt8173.rc \
     $(LOCAL_PATH)/rootdir/init.recovery.mt8173.rc:root/init.recovery.mt8173.rc
