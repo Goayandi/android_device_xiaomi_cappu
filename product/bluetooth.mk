774824f2e65454e3742bda0a834b7a4ba76659a9
goayandi <r.borutha@gmail.com>
2018-10-08 02:00:38 +0200

    fix build error - bluetooth DEVICE_PATH := device/xiaomi/cappu was missing in bluetooth.mk

--- product/bluetooth.mk
+++ product/bluetooth.mk
@@ -1,8 +1,5 @@
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service \
-	libbluetooth_mtk \
-    libbt-vendor
+# Folder path
+DEVICE_PATH := device/xiaomi/cappu
 
 PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/bluetooth/auto_pair_blacklist.conf:system/vendor/etc/bluetooth/auto_pair_blacklist.conf \
@@ -12,4 +9,10 @@ PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/bluetooth/bt_stack.conf:system/vendor/etc/bluetooth/bt_stack.conf \
 	$(DEVICE_PATH)/configs/bluetooth/bt_stack.conf.debug:system/vendor/etc/bluetooth/bt_stack.conf.debug \
 	$(DEVICE_PATH)/configs/bluetooth/bt_stack.conf.sqc:system/vendor/etc/bluetooth/bt_stack.conf.sqc
+
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
+	libbluetooth_mtk \
+    libbt-vendor
 
